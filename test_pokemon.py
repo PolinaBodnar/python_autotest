d4d95f008b8c167e33dc8fe9de2752fd2a8ccee0
Polina Bodnar <honey@list.ru>
2024-01-23 16:43:52 +0200

    Добавлены автотесты

--- test_pokemon.py
+++ test_pokemon.py
@@ -23,4 +23,4 @@ print(response)
 
 def test_get_trainers():
     response = requests.get(url=f'{URL}/trainers', params={'trainer_id ': 3707}, json=body, headers=HEADER, timeout=5)
-    assert response.status_code == 200, 'Unexpected status code'
+    assert response.status_code == 200, 'Unexpected status code'  
